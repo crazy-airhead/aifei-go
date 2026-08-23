1cd038e0092a299c382a292f842ab2e89423a636
crazy-airhead <crazy-airhead@users.noreply.github.com>
2026-08-23 22:46:03 +0000

    docs: deploy f37dccf38f50295b3a5c6f6139afe53977727533 f37dccf38f50295b3a5c6f6139afe53977727533

---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.DdJR6OtP.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.BgspOAAM.js";import{_ as a}from"../app.7yxo5u8t.js";import"./chunk-5VM5RSS4.DNbiHlYa.js";import"./chunk-XXDRQBXY.Cp1EQiCk.js";import"./chunk-POPQ4Y6H.BqyYtLH1.js";import"./chunk-F27PBJKO.MChtxU7n.js";import"./channel.CZumdhPl.js";import"./framework.C66fElYV.js";import"./theme.8LEGYJm4.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
