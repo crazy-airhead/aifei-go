89e2d84cfa56fd41738c718ec894dff0ecab1094
crazy-airhead <crazy-airhead@users.noreply.github.com>
2026-09-14 03:24:08 +0000

    docs: deploy 0b860e4a6c9b7a3945ceeeaf0b5630dfddf1406d 0b860e4a6c9b7a3945ceeeaf0b5630dfddf1406d

---
+++ assets/chunks/swimlanesDiagram-VK2B7HYN.CDWj8H-Y.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.BNzo2YUD.js";import{_ as a}from"../app.Cm_78uGv.js";import"./chunk-5VM5RSS4.B4krMxtU.js";import"./chunk-XXDRQBXY.9qhm0uwW.js";import"./chunk-POPQ4Y6H.qV9cMxZu.js";import"./chunk-F27PBJKO.BEAXHy3m.js";import"./channel.BCMBKd_m.js";import"./framework.C66fElYV.js";import"./theme.Cb0s8zb-.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
